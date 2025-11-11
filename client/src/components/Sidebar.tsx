d80dd7cd1023c7aa53ccffbb0b7b92775d36ceb5
sabalicious <vladchermenin4@gmail.com>
2025-11-11 19:26:48 +0300

    Update Sidebar styles

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -3,8 +3,7 @@ import { NavLink } from "react-router-dom";
 
 const Sidebar: React.FC = () => {
   return (
-    <div>
-      <h1>FinTrack</h1>
+    <nav className="flex gap-2 flex-col pt-20 px-3">
       <NavLink
         to="/"
         className={({ isActive }) =>
@@ -37,7 +36,7 @@ const Sidebar: React.FC = () => {
       >
         Settings
       </NavLink>
-    </div>
+    </nav>
   );
 };
 
