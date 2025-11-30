7946af553b84ddd867ffccf96eef41c0d66f1da3
sabalicious <91668906+sabalicious@users.noreply.github.com>
2025-11-30 16:53:41 +0300

    Remove database connection setup from db.js

--- server/db.js
+++ server/db.js
@@ -1,11 +1,1 @@
-const { Pool } = require('pg');
 
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'fintrack',
-  password: 'JonhLocke4',
-  port: 5432,
-});
-
-module.exports = pool;
